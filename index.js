4a80611630a1c47f684e007c411e6e7bd4d64d8b
Siyang Zhao <kern_zhao@126.com>
2017-01-17 21:23:27 -0500

    Prevent from logging before every send email
    Gmail would block you if you log in too often

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ var nodemailer = require('nodemailer');
 var fs = require('fs');
 var path = require('path');
 
+var _transporter = null;
+
 var config = function(options){
-    var transporter = nodemailer.createTransport('smtps://' + options.email + ':' + options.password + '@' + options.server);
+    if (!_transporter) {
+      var _transporter = nodemailer.createTransport('smtps://' + options.email + ':' + options.password + '@' + options.server);
+    }
     return function(mailOptions){
         return new Promise(function(fullfill, reject){
             var newMailOptions = {};
@@ -41,7 +45,7 @@ var config = function(options){
                   }
                   else{
                       newMailOptions.html = data;
-                      transporter.sendMail(newMailOptions, function(err, info){
+                      _transporter.sendMail(newMailOptions, function(err, info){
                           if(err){
                               reject(err);
                           }
@@ -63,7 +67,7 @@ var config = function(options){
                 if(mailOptions.attachments){
                     newMailOptions.attachments = mailOptions.attachments;
                 }
-                transporter.sendMail(newMailOptions, function(err, info){
+                _transporter.sendMail(newMailOptions, function(err, info){
                     if(err){
                         reject(err);
                     }
@@ -78,74 +82,6 @@ var config = function(options){
 };
 
 module.exports.config = config;
-
-// var config = {
-//     email: 'test@ez4edu.com',
-//     password: 'Xhunter648!',
-//     emailServerDomain: 'dprhcp204.doteasy.com'
-// };
-
-// var transporter = nodemailer.createTransport('smtps://' + config.hostEmailAddress + ':' + config.password + '@' + config.emailServerDomain);
-
-//options example / API
-//{
-//    senderName: 'EZ4.0 EDU',                  // optional, default to 'easy4.0 education'
-//    receivers: ['siyangkernzhao@gmail.com', 'gary.kang@ez4edu.com'],   // can be string or list of string
-//    subjects: 'Please verify email',          //required
-//    html: '<h1>Hello world</h1>',                 //required
-//    attachments: [                                    // can be object or list of object
-//    {
-//        filename: 'hello1.jpg',
-//        path: 'https://s3.amazonaws.com/ezfour/hello1.jpg'
-//    },
-//    {
-//        filename: 'hello2.jpg',
-//        path: 'https://s3.amazonaws.com/ezfour/hello2.jpg'
-//    }]
-//}
-
-// module.exports = function(options){
-//     var mailOptions = {};
-//     var senderName = options.senderName || 'easy4.0 education';
-//     mailOptions.from = `"${senderName}" <${config.hostEmailAddress}>`;
-//     if(!options.receivers){
-//         return console.log('Error: no receivers specified');
-//     }
-//     if (!options.subject){
-//         return console.log('Error: no subject specified');
-//     }
-//     if (!options.html){
-//         return console.log('Error: no contents specified');
-//     }
-//     if(Array.isArray(options.receivers)){
-//         mailOptions.to = options.receivers.join(', ');
-//     }
-//     else{
-//         mailOptions.to = options.receivers;
-//     }
-//     mailOptions.subject = options.subject;
-//     mailOptions.html = options.html;
-//     if (options.attachments){
-//         if(Array.isArray(options.attachments)){
-//             mailOptions.attachments = options.attachments;
-//         }
-//         else{
-//             mailOptions.attachments = [options.attachments];
-//         }
-//     }
-//     return new Promise(function(fullfill, reject){
-//         transporter.sendMail(mailOptions, function(err, info){
-//             if(err){
-//                 reject(err);
-//             }
-//             else{
-//                 fullfill(info);
-//             }
-//         });
-//     });
-// };
-
-//refer to https://nodemailer.com/using-attachments/ for other kind of attachments
 
 
 
